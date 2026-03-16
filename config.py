466ea27bfb99ab3e7371be41220ae4d34c46a40c
Dilshod <turgunboyev@example.com>
2026-03-16 22:56:28 +0500

    update: kod yangilandi

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     BOT_TOKEN: str = os.getenv("BOT_TOKEN")
 
     # config.py ichida
-    DB_HOST = os.getenv("DB_HOST")
+    DB_HOST = os.getenv("DB_HOST","postgres.railway.internal")
     DB_PORT: int = int(os.getenv("DB_PORT"))
     DB_NAME: str = os.getenv("DB_NAME")
     DB_USER: str = os.getenv("DB_USER")
